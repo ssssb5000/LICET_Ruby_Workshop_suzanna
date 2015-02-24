61522df13b313941b9d16cabf29a246ac86067eb
STUDENT <suzanna5000@gmail.com>
2015-02-24 09:55:46 +0530

    added new file

---
+++ rr.rb
@@ -0,0 +1,10 @@
+(1.100).each do|num|
+if(num%3)==0
+puts ("fizz")
+elsif(num%5)==0
+puts ("buzz")
+elsif ((num%3)==0 && (num%5)==0)
+puts ("fizzbuzz")
+end
+end
+
---
+++ simple.rb
@@ -0,0 +1,5 @@
+you="preethi"
+unless you == "preethi"
+puts "good girl"
+end
+
